cff88ce69aa16a2223f2fbe9632b72c43f2c92ef
Lester Jones <lester.jones@rubix.nl>
2021-01-28 10:01:22 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hallo Lester Jones,
+          Edit <code>src/App.js</code> and save to reload. <b/>
+          Klare taal
         </p>
         <a
           className="App-link"
